97463f7494bf8020339bf7505186cf036e2cc4e9
zeeshanilyas <zeeshanilyas002@gmail.com>
2025-02-24 13:00:04 +0500

    FirstProgramInC# for Print Lines

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string str1 = "Kacs";
+string str2 = "Learnings";
+string str3 = string.Concat(str1,str2);
+
+
+Console.WriteLine(str1 +" "+ str2);
+Console.WriteLine("prebuild method : "+ str3);
+Console.WriteLine(str3.ToUpper());
+Console.WriteLine(str3.ToLower());
+Console.WriteLine(str2[0]);
+Console.WriteLine(str2.IndexOf("r"));
+
+
